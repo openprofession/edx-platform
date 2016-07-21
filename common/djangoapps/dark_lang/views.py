0e7bea9d00bdccb60275b595b7a76ae58109e793
Albert St. Aubin <astaubin@edx.org>
2016-07-21 15:37:37 -0400

    Minor PR request (Squash)

--- common/djangoapps/dark_lang/views.py
+++ common/djangoapps/dark_lang/views.py
@@ -149,5 +149,5 @@ class DarkLangView(View):
                 request.session[LANGUAGE_SESSION_KEY] = user_pref
         if user_pref is None:
             return True, _('Language reset to the default language code')
-        return True, _('Language reset to user\'s preference: {preview_language_code}').format(
+        return True, _("Language reset to user's preference: {preview_language_code}").format(
             preview_language_code=user_pref)
